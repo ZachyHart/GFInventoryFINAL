b4b6cbf6e353657151da44ba23d538e1fedbf7a2
Johan Mari Ocampo <johanmari_ocampo@yahoo.com>
2024-04-18 00:07:56 +0800

    removed bg

--- CustomerInventory.php
+++ CustomerInventory.php
@@ -28,16 +28,7 @@ include_once './helpers/session_helper.php';
     <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4/bootstrap-4.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <style>
-        body, html {
-            height: 100%;
-            margin: 0;
-            background-image: url('img/FunkoBgBlur.jpeg');
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-    </style>
+    
 </head>
 
 <body>
